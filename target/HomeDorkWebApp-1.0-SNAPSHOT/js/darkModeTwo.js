733ed66c3c621daa5098fd540531e61f09ab7781
AliHabesh <ali.habesh0068@stud.hkr.se>
2021-12-14 11:42:03 +0100

    cal

--- target/HomeDorkWebApp-1.0-SNAPSHOT/js/darkModeTwo.js
+++
@@ -1,9 +0,0 @@
-const value = localStorage.getItem("theme");
-const html = document.querySelector("html");
-const darkModeFile = document.getElementById("dark");
-const lightModeFile = document.getElementById("dark2");
-if (value.includes("dark")){
-    lightModeFile.remove();
-}else {
-    darkModeFile.remove();
-}
